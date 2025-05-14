abbe0157b29e680a96e7c5bdb2a00863f629af86
231tr0n <zeltronsrikar@gmail.com>
2025-05-15 01:37:21 +0530

    test

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,26 +1,24 @@
 package example
 
-import org.apache.logging.log4j.scala.Logging
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.core.config.Configurator
+import org.apache.logging.log4j.scala.Logging
 
 object Hello extends App with Logging {
-  override def main(args: Array[String]): Unit = {
-    System.getenv("LOGGER_LEVEL") match {
-      case "DEBUG" => Configurator.setRootLevel(Level.DEBUG)
-      case "INFO"  => Configurator.setRootLevel(Level.INFO)
-      case "WARN"  => Configurator.setRootLevel(Level.WARN)
-      case "ERROR" => Configurator.setRootLevel(Level.ERROR)
-      case "FATAL" => Configurator.setRootLevel(Level.FATAL)
-      case "TRACE" => Configurator.setRootLevel(Level.TRACE)
-      case _       => Configurator.setRootLevel(Level.INFO)
-    }
-    println("Hello, world!")
-    logger.info("Hello")
-    logger.warn("Hello")
-    logger.error("Hello")
-    logger.debug("Hello")
-    logger.fatal("Hello")
-    logger.trace("Hello")
+  System.getenv("LOGGER_LEVEL") match {
+    case "DEBUG" => Configurator.setRootLevel(Level.DEBUG)
+    case "INFO"  => Configurator.setRootLevel(Level.INFO)
+    case "WARN"  => Configurator.setRootLevel(Level.WARN)
+    case "ERROR" => Configurator.setRootLevel(Level.ERROR)
+    case "FATAL" => Configurator.setRootLevel(Level.FATAL)
+    case "TRACE" => Configurator.setRootLevel(Level.TRACE)
+    case _       => Configurator.setRootLevel(Level.INFO)
   }
+  println("Hello, world!")
+  logger.info("Hello")
+  logger.warn("Hello")
+  logger.error("Hello")
+  logger.debug("Hello")
+  logger.fatal("Hello")
+  logger.trace("Hello")
 }
